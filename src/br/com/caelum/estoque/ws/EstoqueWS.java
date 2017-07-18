bcca4d04576a8787c0f417daa2d3294d45cc7934
douglas.chagas <douglas.chagas@gmail.com>
2017-07-18 15:40:21 -0300

    Aula 2

--- src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -1,19 +1,38 @@
 package br.com.caelum.estoque.ws;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
+import br.com.caelum.estoque.modelo.item.Filtro;
+import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
+import br.com.caelum.estoque.modelo.item.ListaItens;
 
 @WebService
 public class EstoqueWS {
 
 	private ItemDao dao = new ItemDao();
 
+	@WebMethod(operationName="todosOsItens")
+	@WebResult(name="itens")
+	public ArrayList<Item> getItens(Filtros filtros) { //cuidado, plural
+        System.out.println("Chamando getItens()");
+        List<Filtro> lista = filtros.getLista();
+        return dao.todosItens(lista);
+        //List<Item> itensResultado = dao.todosItens(lista);
+        //return new ListaItens(itensResultado);
+    }
+    
+	
+	/*
     public List<Item> todosItens() {
         System.out.println("Chamando todosItens()");
         return dao.todosItens();
     }
+    */
 }
